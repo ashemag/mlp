06c04723c7c3dc946dd2331b251e9d4e3766a76a
Ashe Magalhaes <ashemagalhaes@Ashes-MacBook-Pro.local>
2019-01-24 16:50:48 +0000

    baseline attack class on log regression model of mnist

---
+++ baseline_attack.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Attack Logistic Regression classification of MNIST Dataset
+
+Author: Ashe Magalhaes
+Email: ashe.magalhaes@gmail.com
+Reference: https://www.kaggle.com/allunia/how-to-attack-a-machine-learning-model/data
+"""
+import pandas as pd
+
+
