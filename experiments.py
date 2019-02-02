5a6c4abf07ea43bdaf6fd39baef4f6868e45a4ba
Ashe Magalhaes <ashemagalhaes@Ashes-MacBook-Pro.local>
2019-02-02 11:41:05 +0000

    syncing w martin's changes

--- experiments.py
+++ experiments.py
@@ -1,12 +1,13 @@
-from mlp.data_providers import MNISTDataProvider, ModifyDataProvider
+from mlp.data_providers import MNISTDataProvider, EMNISTDataProvider, ModifyDataProvider
 from mlp.models import MultipleLayerModel
 from mlp.layers import ReluLayer, AffineLayer
 from mlp.errors import CrossEntropySoftmaxError
 from mlp.initialisers import GlorotUniformInit, ConstantInit
-from mlp.learning_rules import MomentumLearningRule
+from mlp.learning_rules import AdamLearningRule
 from mlp.optimisers import Optimiser
 import numpy as np
 import os
+import torch
 os.environ['MLP_DATA_DIR'] = 'data'
 
 
@@ -17,7 +18,7 @@ class Experiment(object):
 
         # Use a momentum learning rule - you could use an adaptive learning rule
         # implemented for the coursework here instead
-        learning_rule = MomentumLearningRule(0.02, 0.9)
+        learning_rule = AdamLearningRule()
         # Monitor classification accuracy during training
         data_monitors = {'acc': lambda y, t: (y.argmax(-1) == t.argmax(-1)).mean()}
 
@@ -31,21 +32,25 @@ class Experiment(object):
         return err_train, err_valid, acc_train, acc_valid
 
     def _compare(self, model, target_class, target_percentage, num_epochs):
-        train_data = MNISTDataProvider('train')
-        valid_data = MNISTDataProvider('valid')
+        train_data = MNISTDataProvider('train', batch_size=100)
+        valid_data = MNISTDataProvider('test', batch_size=100)
 
         err_train, err_valid, acc_train, acc_valid = self._train(model, num_epochs, train_data, valid_data)
 
-        inputs, targets = ModifyDataProvider().modify(target_class, target_percentage, train_data.inputs, train_data.targets)
+        m = ModifyDataProvider()
+        m.get_label_distribution(train_data.targets)
+        inputs, targets = m.modify(target_class, target_percentage, train_data.inputs, train_data.targets)
         train_data.inputs = np.array(inputs)
         train_data.targets = np.array(targets)
+        m.get_label_distribution(targets)
+
         err_train_comp, err_valid_comp, acc_train_comp, acc_valid_comp = self._train(model, num_epochs, train_data, valid_data)
 
         acc_valid_diff = ((acc_valid_comp - acc_valid) / float(acc_valid_comp)) * 100
         acc_train_diff = ((acc_train_comp - acc_train) / float(acc_train_comp)) * 100
         print("\n===\n")
-        print("Training accuracy decreased by {0}%".format(round(acc_train_diff, 2)))
-        print("Validation accuracy decreased by {0}%".format(round(acc_valid_diff, 2)))
+        print("Training accuracy changed by {0}%".format(round(acc_train_diff, 2)))
+        print("Validation accuracy changed by {0}%".format(round(acc_valid_diff, 2)))
         print("\n===\n")
 
     def play(self, target_class, target_percentage):
@@ -53,22 +58,21 @@ class Experiment(object):
         seed = 31102016
         rng = np.random.RandomState(seed)
 
-        # Probability of input being included in output in dropout layer
-        incl_prob = 0.5
-        input_dim, output_dim, hidden_dim = 784, 10, 125
+        # setup hyperparameters
+        num_epochs = 100
+        input_dim, output_dim, hidden_dim = 784, 10, 100
 
-        # Use Glorot initialisation scheme for weights and zero biases
-        weights_init = GlorotUniformInit(rng=rng, gain=2. ** 0.5)
+        weights_init = GlorotUniformInit(rng=rng)
         biases_init = ConstantInit(0.)
-
-        # Create three affine layer model with rectified linear non-linearities
-        # and dropout layers before every affine layer
-        model = MultipleLayerModel([
-            AffineLayer(input_dim, hidden_dim, weights_init, biases_init),
-            ReluLayer(),
-            AffineLayer(hidden_dim, output_dim, weights_init, biases_init)
-        ])
-        self._compare(model, target_class, target_percentage, 2)
+        # model = MultipleLayerModel([
+        #     AffineLayer(input_dim, hidden_dim, weights_init, biases_init),
+        #     ReluLayer(),
+        #     AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init),
+        #     ReluLayer(),
+        #     AffineLayer(hidden_dim, output_dim, weights_init, biases_init)
+        # ])
+        model = torch.nn.Linear(input_dim, output_dim)
+        self._compare(model.parameters(), target_class, target_percentage, num_epochs)
 
 
 Experiment().play(0, .01)
